c33a96a69b2c37fb28faa95065e7cab4e3b882d6
Hans Alemão <aulasparticularesdealemaosp@gmail.com>
2024-04-02 23:02:13 -0300

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
-import re
+import json
 import random
+import re
+import time
 import requests
 
 ipreg = re.compile(
@@ -7,20 +9,26 @@ ipreg = re.compile(
 )
 
 
-def _getip(link, action):
+def _getip(link, action, *args, **kwargs):
     result = None
+    if "timeout" not in kwargs:
+        kwargs["timeout"] = 5
     try:
-        with requests.get(link) as fa:
-            result = action(fa)
-            result = ipreg.findall(result)[0]
-    except Exception:
+        with requests.get(link, **kwargs) as fa:
+            result = fa.content
+    except Exception as e:
+        pass
+    try:
+        result = action(result.decode("utf-8", "replace"))
+        result = ipreg.findall(result)[0]
+    except Exception as e:
         pass
     return result
 
 
-def get_ip_of_this_pc():
-    fu1 = lambda fa: fa.text.strip()
-    fu2 = lambda fa: fa.json()["ip"].strip()
+def get_ip_of_this_pc(*args, **kwargs):
+    fu1 = lambda fa: fa.strip()
+    fu2 = lambda fa: json.loads(fa)["ip"].strip()
     sites_and_actions = [
         ("https://checkip.amazonaws.com", fu1),
         ("https://api.ipify.org", fu1),
@@ -32,7 +40,47 @@ def get_ip_of_this_pc():
         ("https://www.trackip.net/ip", fu1),
     ]
     random.shuffle(sites_and_actions)
+    result = None
     for link, action in sites_and_actions:
-        result = _getip(link, action)
+        try:
+            result = _getip(link, action, *args, **kwargs)
+            result = ipreg.findall(result)[0]
+        except Exception as e:
+            pass
         if result:
             return result
+
+
+def doiploop(*args, **kwargs):
+    r"""
+    This function takes in variable positional and keyword arguments, and it retrieves
+    the WAN IP address of the current PC.
+    It also allows for a loop timeout (looptimeout) to be specified.
+    The function returns the WAN IP address if it is found, and returns None if a
+    timeout occurs or an exception is encountered.
+    kwargs are passed to requests.get()
+
+    print(doiploop())
+    print(
+        doiploop(
+            proxies={
+                "https": "socks5://111.113.119.110:53681",
+                "http": "socks5://111.113.119.110:53681",
+            }
+        )
+    )
+    """
+    looptimeout = kwargs.pop("looptimeout", 0)
+    try:
+        mywanip = get_ip_of_this_pc(*args, **kwargs)
+        if looptimeout:
+            timeoutfinal = time.time() + looptimeout
+        while not re.match(r"^\d+\.\d+\.\d+\.\d+$", mywanip):
+            if looptimeout:
+                if time.time() > timeoutfinal:
+                    return None
+            mywanip = get_ip_of_this_pc(*args, **kwargs)
+    except Exception as fe:
+        pass
+    return mywanip
+
